637422b7a2ecf184d03f22d2107e22d1ad8c30ad
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2022-09-20 16:57:21 +0100

    password

---
+++ index.js
@@ -0,0 +1,27 @@
+// Function for generating random number
+const genNumber = (num2) => {
+  return Math.floor(Math.random() * 100) % (num2 + 1);
+};
+
+// Function for generating the password
+const generatePassword = (length) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+const createPassword = (num1, num2) => {
+  let number = genNumber(num2);
+  while (number < num1) {
+    number = genNumber(num2);
+  }
+  console.log(number);
+  return generatePassword(number);
+};
+
+module.exports = createPassword;
